90652ce096acf2a7fbe74e038a9a5179bec1aa8c
Clayton Siby <claytonsiby@gmail.com>
2023-05-16 19:14:30 +0200

    Add: express server to handle realtime data handling

---
+++ server.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const PORT = 3000 || process.env.PORT;
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+const botName = "SibyChat Bot";
+// run when a client connects
+io.on("connect", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+    // welcome current user
+    socket.emit("message", formatMessage(botName, "Welcome to SibyChat"));
+
+    // broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${username} has joined the chat!`)
+      );
+
+    // send users in room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // listen for chatMessage
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  // runs when client disconnects
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // send users in room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
